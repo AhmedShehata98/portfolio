d6aa0bb10722058ffc1900e90e6c41c7787a5c88
Ahmed Shehata <ahmedshehataq98@gmail.com>
2025-07-21 15:40:16 +0300

    Enhance metadata with Open Graph and Twitter tags
    Added Open Graph and Twitter metadata to improve SEO and social sharing. Also included canonical URL and robots directives in the metadata object for better search engine handling.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,26 +5,65 @@ import GallerySection from "@/components/sections/Gallery";
 import TimelineSection from "@/components/sections/Timeline";
 import ContactSection from "@/components/sections/Contact";
 import GoTopBtn from "@/components/GoTopBtn";
-import {setRequestLocale} from "next-intl/server";
-import {routing} from "@/i18n/routing";
-import {use} from "react";
+import { setRequestLocale } from "next-intl/server";
+import { routing } from "@/i18n/routing";
+import { use } from "react";
 
 export function generateStaticParams() {
-    return routing.locales.map((locale) => ({ locale }));
+  return routing.locales.map((locale) => ({ locale }));
 }
 
 export const metadata: Metadata = {
   title: "Ahmed Shehata | Portfolio",
   description:
     "Passionate frontend developer specializing in modern web technologies and creating exceptional user experiences",
+  openGraph: {
+    title: "Ahmed Shehata | Portfolio",
+    description:
+      "Passionate frontend developer specializing in modern web technologies and creating exceptional user experiences",
+    url: "https://ahmedshehata.online",
+    images: [
+      {
+        url: "/profile-photo.png",
+        width: 375,
+        height: 468,
+        alt: "ahmed-shehata-profile-img.png",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ahmed Shehata | Portfolio",
+    description:
+      "Passionate frontend developer specializing in modern web technologies and creating exceptional user experiences",
+    images: [
+      {
+        url: "/profile-photo.png",
+        width: 375,
+        height: 468,
+        alt: "ahmed-shehata-profile-img.png",
+        type: "image/png",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "https://ahmedshehata.online",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
 };
 
-export default function HomePage({params}: {
-    params: Promise<{ locale: string }>;
+export default function HomePage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
 }) {
-    const { locale } = use(params);
-
-    setRequestLocale(locale || routing.defaultLocale);
+  const { locale } = use(params);
+  setRequestLocale(locale || routing.defaultLocale);
 
   return (
     <main className="relative w-full min-h-screen flex flex-col items-center justify-center">
